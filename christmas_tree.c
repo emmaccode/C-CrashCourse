4c9dc3115d75bbfd8a4984f9428e03fbe17e5129
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2021-05-31 09:34:42 -0400

    it is christmas time

---
+++ christmas_tree.c
@@ -0,0 +1,13 @@
+// Isosceles!
+#include <stdlib.h>
+#include <stdio.h>
+int main() {
+    printf("    ★\n");
+    printf("   /_\\\n");
+    printf("  /_ _\\\n");
+    printf(" /_  _ \\\n");
+    printf("/       \\\n");
+    printf("---------\n");
+    printf("   | |\n");
+    return 0;
+}
